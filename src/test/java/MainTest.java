99d3cd325057c90fcdbb405da172d0f3dbe6ccdf
vivek.borsania <vivek.borsania@automationanywhere.com>
2020-04-16 14:25:06 +0530

    Main and TestMain class added

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,23 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("setUp() Method called - MainTest");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("tearDown() Method called - MainTest");
+    }
+
+    @Test
+    public void main() {
+        System.out.println("main() Method called - MainTest");
+    }
+}
